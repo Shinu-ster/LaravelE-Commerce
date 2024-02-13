49ce64f7b3ef28781e3e828aa579d53ed1c56f31
shinu-ster <sajatbazz@gmail.com>
2024-02-13 19:03:10 +0545

    Initial commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,17 @@
+
+import {Button} from 'react-bootstrap'
+import NavBar from './components/NavBar'
+function App() {
+
+
+  return (
+    <>
+    <NavBar/>
+    <h1>Hello</h1>
+    <button>Html button</button>
+    <Button>BootStrap</Button>
+    </>
+  )
+}
+
+export default App
